35260cac463a1222bd10db9506aac4300f58c9eb
ckab04 <ciceronkapenabm@gmail.com>
2024-02-11 23:22:27 +0100

    Read image file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
 use crate::hardware::Registers;
 use crate::hardware::Registers::*;
 
@@ -18,6 +21,20 @@ fn main() {
     println!("#{:06x}", trap_codes::TRAP::TrapGet as i32);
 }
 
+
+fn read_image_file(file_path: PathBuf){
+
+    let mut content = String::new();
+    let mut file = File::open(file_path).unwrap();
+    let origin = file.read_to_string(&mut content).unwrap();
+
+    let origin = swap16(origin as u16);
+    /* we know the maximum file size so we only need one fread */
+}
+
+fn swap16(x: u16)-> u16{
+    (x << 8 ) | (x >> 8)
+}
 
 
 
